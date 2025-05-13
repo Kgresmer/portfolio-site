b32b456787b1903df3e22c0a1f90dd5fc8209246
Kevin Gresmer <kgresmer@gmail.com>
2025-05-13 06:06:18 -0500

    Adding hero and spotlights

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,42 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import type {Metadata} from "next";
+import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
+import {ThemeProvider} from "./provider";
 
 const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
+    variable: "--font-geist-sans",
+    subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
+    variable: "--font-geist-mono",
+    subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: "Kevin Gresmer's Portfolio",
-  description: "A website to showcase who I am and my past experience.",
+    title: "Kevin Gresmer's Portfolio",
+    description: "A website to showcase who I am and my past experience.",
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+                                       children,
+                                   }: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en" suppressHydrationWarning>
+            <body
+                className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+            >
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="dark"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {children}
+                </ThemeProvider>
+            </body>
+        </html>
+    );
 }
---
+++ components/Hero.tsx
@@ -0,0 +1,38 @@
+import {Spotlight} from "@/components/ui/Spotlight";
+import {cn} from "@/lib/utils";
+
+const Hero = () => {
+    return (
+        <div className="pb-20 pt-36">
+            <div>
+                <Spotlight className="-top-40 -left-40 md:left-12 md:-top-20 h-screen" fill="white"/>
+                <Spotlight className="top-12 left-70 h-[80vh] w-[50vw] md:left-80" fill="purple"/>
+                <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue"/>
+            </div>
+
+            <div className="flex h-screen w-full items-center justify-center dark:bg-black-100 absolute top-0 left-0">
+                <div
+                    className={cn(
+                        "absolute inset-0",
+                        "[background-size:40px_40px]",
+                        "[background-image:linear-gradient(to_right,blue_1px,transparent_1px),linear-gradient(to_bottom,purple_1px,transparent_1px)]",
+                    )}
+                />
+                {/* Radial gradient for the container to give a faded look */}
+                <div
+                    className="pointer-events-none absolute inset-0 flex items-center justify-center bg-black [mask-image:radial-gradient(ellipse_at_center,transparent_15%,black)] dark:bg-black" />
+            </div>
+
+            <div className="flex justify-center relative my-20 z-10">
+                <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
+                    <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+                        Dynamic Web Magic with Next.js
+                    </h2>
+                </div>
+
+            </div>
+        </div>
+    )
+}
+
+export default Hero;
