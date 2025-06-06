3c3d959d7e984aecaea5614677fa905ba9b91d71
Kevin Gresmer <kgresmer@gmail.com>
2025-06-06 14:23:15 -0500

    Adding cool globe to show timezone availability.

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,4 +1,6 @@
 import {cn} from "@/lib/utils";
+import {BackgroundGradientAnimation} from "./BackgroundGradientAnimation";
+import { Globe } from "./Globe";
 
 export const BentoGrid = ({
                               className,
@@ -48,13 +50,35 @@ export const BentoGridItem = ({
                 background: 'linear-gradient(90deg,rgba(2, 0, 36, 1) 0%, rgba(9, 9, 121, 1) 35%, rgba(115, 0, 27, 1) 100%)'
             }}
         >
-            <div className="transition duration-200 group-hover/bento:translate-x-2">
-                <div className="mt-2 mb-2 font-sans font-bold">
-                    {title}
+            <div className={`${id === 6} && 'flex justify-center h-full'`}>
+                <div className="w-full h-full absolute">
+                    {img && (
+                        <img src={img} alt={img} className={cn(imgClassName, "object-cover object-center")}/>
+                    )}
                 </div>
-                <div className="font-sans text-xs font-normal">
-                    {description}
+                <div className={`absolute right-0 -bottom-5 ${id === 5 && 'w-full opacity-80'}`}>
+                    {spareImg && (
+                        <img src={spareImg} alt={spareImg} className="object-cover object-center w-full h-full"/>
+                    )}
                 </div>
+                {id === 6 && (
+                    <BackgroundGradientAnimation>
+                        <div className="absolute z-50 flex items-center justify-center text-white font-bold"/>
+                    </BackgroundGradientAnimation>
+                )}
+
+                <div
+                    className={cn(titleClassName,
+                        "group-hover relative md:h-full min-h-40 flex flex-col px-5 p-5 lg:p-10 group-hover/bento:translate-x-2 transition duration-200")}>
+                    <div className="font-sans text-sm md:text-xs lg:text-base z-10 font-extralight text-[#c1c2d3]">
+                        {description}
+                    </div>
+                    <div className="font-sans font-bold text-lg lg:text-3xl max-w-96 z-10">
+                        {title}
+                    </div>
+                </div>
+
+                {id === 2 && <Globe />}
             </div>
         </div>
     );
